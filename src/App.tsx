93dd5b1c407d0e6eb708560094df6f0062e6682b
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-03-02 17:23:34 +0100

    dynamic search for the pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Card, CardHeader, CardContent, CardMedia, TextField, Typography, Button, Box } from '@mui/material';
 import { useRef, useState } from 'react';
 
+interface IPokemon {
+  id: number,
+  name: string
+}
+
 function App() {
   const searchValue = useRef<HTMLInputElement>()
-  const [isPokemon, setIsPokemon] = useState()
+  const [isPokemon, setIsPokemon] = useState<IPokemon>()
 
   function handleClick() {
-    fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${searchValue.current?.value}`)
+    fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${searchValue.current?.value.toLowerCase()}`)
       .then((data) => data.json())
       .then((pokemon) => setIsPokemon(pokemon))
 
@@ -28,14 +33,16 @@ function App() {
             </>
           }>
           </CardHeader>
+          { isPokemon && 
           <CardContent>
-            <Typography>Pikachu #25</Typography>
-            <CardMedia sx={{ height: 150, width: 150 }} image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" title="Poke" />
+            <Typography>{isPokemon.name.toUpperCase()} #{isPokemon.id}</Typography>
+            <CardMedia sx={{ height: 150, width: 150 }} image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${isPokemon.id}.png`} title="Poke" />
             <Box display={"flex"} alignItems={"center"} flexDirection={"row"}>
               <Typography sx={{ backgroundColor: "yellow", fontSize: "1rem", borderRadius: "5px", width: "fit-content", padding: "5px", marginRight: "5px" }}>ELECTRIC</Typography>
               <Typography sx={{ backgroundColor: "red", fontSize: "1rem", borderRadius: "5px", width: "fit-content", padding: "5px", marginRight: "5px" }}>FIRE</Typography>
             </Box>
           </CardContent>
+          }
         </Card>
       </Box>
     </>
