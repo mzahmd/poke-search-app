58ee293096070f26ebde60c38483b0a727c1da2f
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-03-02 18:29:22 +0100

    add pokemon stats

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Card, CardHeader, CardContent, CardMedia, TextField, Typography, Button, Box } from '@mui/material';
+import { Card, CardHeader, CardContent, CardMedia, TextField, Typography, Button, Box, Grid } from '@mui/material';
 import { useRef, useState } from 'react';
 interface IPokemon {
   id: number,
   name: string
   types: { type: { name: string } }[]
+  stats: { base_stat: number, stat: { name: string } }[]
 }
 
 function App() {
@@ -15,11 +16,8 @@ function App() {
       .then((data) => data.json())
       .then((pokemon) => {
         setIsPokemon(pokemon)
-        isPokemon?.types.map(type => console.log(type.type.name))
         console.log(isPokemon);
       })
-
-
   }
 
   return (
@@ -35,16 +33,36 @@ function App() {
             </>
           }>
           </CardHeader>
-          <CardContent sx={{minHeight: "50vh", backgroundImage:"radial-gradient(circle at 18.7% 37.8%, rgb(250, 250, 250) 0%, rgb(225, 234, 238) 90%)"}}>
+          <CardContent sx={{ minHeight: "20rem",width:"30rem" , backgroundImage: "radial-gradient(circle at 18.7% 37.8%, rgb(250, 250, 250) 0%, rgb(225, 234, 238) 90%)" }}>
             {isPokemon &&
               <>
                 <Typography>{isPokemon.name.toUpperCase()} #{isPokemon.id}</Typography>
                 <CardMedia sx={{ height: 150, width: 150 }} image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${isPokemon.id}.png`} title="poke" />
                 <Box display={"flex"} alignItems={"center"} flexDirection={"row"}>
-                  {isPokemon?.types.map(type =>
-                    <Typography sx={{ backgroundColor: "red", fontSize: "1rem", borderRadius: "5px", width: "fit-content", padding: "5px", marginRight: "5px" }}>{type.type.name}</Typography>
+                  {isPokemon?.types.map(({ type }) =>
+                    <Typography sx={{ backgroundColor: "red", fontSize: "1rem", borderRadius: "5px", width: "fit-content", padding: "5px", marginRight: "5px" }}>{type.name}</Typography>
                   )}
                 </Box>
+                <Grid container spacing={1} sx={{ textAlign: "center", marginTop: "1rem" }}>
+                  <Grid item xs={8}>
+                    <Typography sx={{ backgroundColor: "lightgray", padding: "5px" }}>Base</Typography>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Typography sx={{ backgroundColor: "lightgray", padding: "5px" }}>Stats</Typography>
+                  </Grid>
+                  {isPokemon.stats.map(stats => {
+                    return (
+                      <>
+                        <Grid item xs={8}>
+                          <Typography sx={{ backgroundColor: "lightgray", padding: "5px" }}>{stats.stat.name}:</Typography>
+                        </Grid>
+                        <Grid item xs={4}>
+                          <Typography sx={{ backgroundColor: "lightgray", padding: "5px" }}>{stats.base_stat}</Typography>
+                        </Grid>
+                      </>
+                    )
+                  })}
+                </Grid>
               </>
             }
           </CardContent>
