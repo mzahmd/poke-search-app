593ae73c39ab3a815cdaebedab3880709e4b4939
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-03-02 15:41:39 +0100

    build the search field and card content

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
+import { CardMedia, Typography, Button, Box } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import TextField from '@mui/material/TextField';
+import CardHeader from '@mui/material/CardHeader';
 
+{/* <img src='https://raw.github usercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png' /> */ }
 function App() {
 
   return (
-    <h1>Pokemon Search App</h1>
+    <>
+      <Box display={"flex"} alignItems={"center"} justifyContent={"center"} flexDirection={"column"}>
+        <h1>Pokémon Search App</h1>
+
+        <Card>
+          <CardHeader action={
+            <>
+              <Typography>Search for Pokémon Name or ID:</Typography>
+              <TextField id="outlined-search" type="search" />
+              <Button variant="contained" sx={{ marginLeft: "10px" }}>search</Button>
+            </>
+          }>
+          </CardHeader>
+          <CardContent>
+            <Typography>Pikachu #25</Typography>
+            <CardMedia sx={{ height: 150, width: 150 }} image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" title="Poke" />
+
+          </CardContent>
+        </Card>
+      </Box>
+    </>
   )
 }
 
